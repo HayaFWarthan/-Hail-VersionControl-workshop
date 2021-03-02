414cc04aea7f357700e54aaf8cf3f026f48d2039
HayaFWarthan <79941196+HayaFWarthan@users.noreply.github.com>
2021-03-03 00:23:29 +0300

    Create Sort binary tree by levels.java
    function to sort binary tree written in java, first commit

---
+++ Sort binary tree by levels.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+class Kata {
+  static public List<Integer> treeByLevels (Node node ){
+        if(node==null)
+  return Collections.emptyList();
+      List<Node> list = new ArrayList<Node>(); 
+        List<Integer> vals = new ArrayList<Integer>(); 
+      list.add(node);
+    for(int i=0;i<list.size();i++){
+    if(list.get(i).left!=null)
+      list.add(list.get(i).left);
+   if(list.get(i).right!=null)
+      list.add(list.get(i).right);       }
+    
+          for(int i=0;i<list.size();i++)
+          vals.add(list.get(i).value);
+    
+  return vals;
+  
+  } 
+  }
